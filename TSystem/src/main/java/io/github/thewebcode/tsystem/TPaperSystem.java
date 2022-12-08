b96eb51032f49b05e129021a26e04a4c95ff4207
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2022-12-09 00:03:10 +0100

    Add a JavaDoc
    Also add Comments

--- TSystem/src/main/java/io/github/thewebcode/tsystem/TPaperSystem.java
+++ TSystem/src/main/java/io/github/thewebcode/tsystem/TPaperSystem.java
@@ -6,24 +6,47 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.Messenger;
 
 public class TPaperSystem extends JavaPlugin {
+    //The TPaperSystem also know as instance
     private static TPaperSystem instance;
+    
+    //The TAPI
     private TAPI api;
 
+    /** 
+     * On load
+     */
     @Override
     public void onLoad() {
+        //Initialize the instance
         instance = this;
+        
+        //Initialize the TAPI
         this.api = new TAPI(true);
     }
 
+    /** 
+     * On enable
+     */
     @Override
     public void onEnable() {
+        //Register the IncomingPluginChannel
         getServer().getMessenger().registerIncomingPluginChannel(this, "tsystem:main", new MessageListener());
     }
 
+    /**
+     * Gets the api
+     *
+     * @return the api
+     */
     public TAPI getApi() {
         return api;
     }
 
+    /** 
+     * Gets the instance
+     *
+     * @return the instance
+     */
     public static TPaperSystem getInstance() {
         return instance;
     }
