f06ab3e1551c9880ee4cddbbcd62080feee6e92d
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2022-12-08 23:59:48 +0100

    Add a JavaDoc
    Also added Comments to this class.

--- TSystem/src/main/java/io/github/thewebcode/tsystem/TBungeeSystem.java
+++ TSystem/src/main/java/io/github/thewebcode/tsystem/TBungeeSystem.java
@@ -8,25 +8,52 @@ import net.md_5.bungee.api.plugin.Plugin;
 import java.util.concurrent.TimeUnit;
 
 public final class TBungeeSystem extends Plugin implements Listener {
+    
+    //The TBungeeSystem (also now as the instance)
     private static TBungeeSystem instance;
+    
+    //The TAPI
     private TAPI api;
 
+    /** 
+     * On load
+     */
     @Override
     public void onLoad() {
+        //Initialize the instance
         instance = this;
+        
+        //Initialize the api
         this.api = new TAPI(false);
     }
 
+    /** 
+     * On enable
+     */
     @Override
     public void onEnable() {
+        //Register the channel
         getProxy().registerChannel("tsystem:main");
+        
+        //Register the Listener
         getProxy().getPluginManager().registerListener(this, new BungeeMessageListener());
     }
 
+
+    /** 
+     * Gets the TAPI
+     *
+     * @return the TAPI
+     */
     public TAPI getApi() {
         return api;
     }
 
+    /** 
+     * Gets the instance
+     *
+     * @return the instance
+     */
     public static TBungeeSystem getInstance() {
         return instance;
     }
