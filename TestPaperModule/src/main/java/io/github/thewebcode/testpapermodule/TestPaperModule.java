a7fccce2f7aa71d620e074d97c9457223a748fa8
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2022-12-09 14:39:56 +0100

    CleanUp Code + add Comments

--- TestPaperModule/src/main/java/io/github/thewebcode/testpapermodule/TestPaperModule.java
+++ TestPaperModule/src/main/java/io/github/thewebcode/testpapermodule/TestPaperModule.java
@@ -12,25 +12,61 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
+@SuppressWarnings("unused")
 public final class TestPaperModule extends JavaPlugin implements CommandExecutor {
+
+    //Defined the TestPaperModule
     private static TestPaperModule instance;
 
+    /**
+     * The onEnable Function
+     */
     @Override
     public void onEnable() {
+
+        //Initialize the instance
         instance = this;
-        Module m = new Module();
-        TPaperSystem.getInstance().getApi().registerModule(m);
-        getCommand("test").setExecutor(this);
+
+        //Initialize the Module
+        Module module = new Module();
+
+        //Register the module
+        TPaperSystem.getInstance().getApi().registerModule(module);
+
+        //Register the command
+        Objects.requireNonNull(getCommand("test")).setExecutor(this);
+
+        //Register the OutgoingPluginChannel
         getServer().getMessenger().registerOutgoingPluginChannel(this, "tsystem:main");
-        MethodMap map = TAPI.map(m, TestClass.class);
-        TAPI.get().register(map);
+
+        //Initialize the MethodMap
+        MethodMap methodMap = TAPI.map(module, TestClass.class);
+
+        //Register the methodMap
+        TAPI.get().register(methodMap);
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        //Initialize the ByteArrayDataOutput
         ByteArrayDataOutput output = ByteStreams.newDataOutput();
+        
+        //Write the UTF
         output.writeUTF("hello");
+        
+        //Send a Message
         sender.getServer().sendPluginMessage(this, "tsystem:main", output.toByteArray());
         return true;
+    }
+
+    /**
+     * Returns the {@link TestPaperModule} instance
+     * @return Returns the instance
+     */
+    public static TestPaperModule getInstance(){
+        return instance;
     }
 }
